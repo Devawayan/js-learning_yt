274f2e6cd742f8da076255c76205738ecb90d3f2
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-08-27 08:36:34 +0000

    conversion

---
+++ 01_Basics/03_Conversion_Opreation.js
@@ -0,0 +1,13 @@
+let score = "33abc"
+
+console.log(score);
+console.log(typeof(score));
+
+
+//when the Number is converted it gets converted to NAN (Not a number)..
+//It shows Number converted but it converts it to NAN.
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+//
