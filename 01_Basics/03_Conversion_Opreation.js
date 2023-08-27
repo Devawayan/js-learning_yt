700a79c1a4317d04949a0b8e587770eca1bfee07
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-08-27 13:40:54 +0000

    Conversion of Data types

--- 01_Basics/03_Conversion_Opreation.js
+++ 01_Basics/03_Conversion_Opreation.js
@@ -10,4 +10,28 @@ let valueInNumber=Number(score)
 console.log(typeof(valueInNumber));
 console.log(valueInNumber);
 
-//
+
+//converting number into Bollean
+
+let isLoggedIn=1
+console.log(typeof(isLoggedIn));
+let BolleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(BolleanIsLoggedIn);
+console.log(isLoggedIn);
+console.log(typeof(BolleanIsLoggedIn));
+
+/*
+"33" => 33
+"33abc"=> NAN
+
+in bollean
+true=1, false=0
+1=> true , 0=>false "1"or any value in double quotes =>true, empty string=>false
+
+you can easily convert the Data types into another data types.
+*/
+
+let someNumber=69
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
