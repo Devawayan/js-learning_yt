a065ee40423bbbb6a179f2d0ff1dd618d7f5169d
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-08-27 05:41:52 +0000

    changes in data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+let Name="Devawayan"
+let age=20
+let loggedin=true
+let temp=null
+let me=undefined
+
+console.table([Name,age,loggedin,temp,me]);
+
+/*
+The data types In Javascript is:
+String
+int 
+bigInt
+boolean => True/Flase
+null=> Standalone Value it comes under object data type
+undefined => it is a type of data type in JS.
+symbol => we use it for unique
+object=> data type 
+*/
+
+//we use tyeof  to know about the type of the data types.
+
+console.log(typeof null);
+console.log(typeof Symbol);
+console.log(typeof Object);
+console.log(typeof undefined);
