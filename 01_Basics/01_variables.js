749f7c5b0b20ff59d0c6436ffc4ccd12c7108353
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-08-27 05:22:39 +0000

    changes

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -8,7 +8,7 @@ let accountState        //if u leave a variable undefined javascript tells it un
 
 // accountId=6869  we cannot Cahnge The Value of Const.
 
-//we use let and not var because of the Scope in functional and scope declaration
+//we use let and not var because of the issue in block scope and functional scope
 
 accountName="Mithu"
 accountEmail="mithuhore89@gmail.com"
