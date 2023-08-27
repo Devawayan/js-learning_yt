4dfa74fa105f9cf05f6fd9311a13ee480a013dff
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-08-27 05:19:52 +0000

    variables declaration and rules

---
+++ 01_Basics/variables.js
@@ -0,0 +1,18 @@
+const accountId=6968
+let accountName = "Devawayan"
+let accountEmail="devawayanhore969@gmail.com"
+city="Jamshedpur"       //we should avoid this way of defining variables..
+let accountState        //if u leave a variable undefined javascript tells it undefined..
+
+//Changing The Values here.
+
+// accountId=6869  we cannot Cahnge The Value of Const.
+
+//we use let and not var because of the Scope in functional and scope declaration
+
+accountName="Mithu"
+accountEmail="mithuhore89@gmail.com"
+city="Kolkata"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountName,city, accountState])
