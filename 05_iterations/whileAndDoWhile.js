1ee2f13d615928a577c6d75483cfd23ef30df5a8
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-09-16 14:40:09 +0000

    While and Do while loop in JS

---
+++ 05_iterations/whileAndDoWhile.js
@@ -0,0 +1,25 @@
+//While Loop 
+
+i=0
+while (i<=10) {
+    // console.log(`Value of the i is ${i}`);
+    i=i+2
+}
+
+//Array from While Loop
+
+const myArray=['flash','batman','superman','spiderman']
+let arr=0
+while (arr<myArray.length) {
+    console.log(`The Value inside the array is ${myArray[arr]}`);
+    arr++
+}
+
+//Do while Loop
+
+let score=1
+
+do {
+    console.log(`The Score is ${score}`); 
+    score++   
+} while (score<=10);
