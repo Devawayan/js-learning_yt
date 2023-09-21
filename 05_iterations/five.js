5b2c73b097c4aa264a576f0b8b02cd56384c9d12
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-09-21 13:12:23 +0000

    For each loop for Arrays

---
+++ 05_iterations/five.js
@@ -0,0 +1,13 @@
+//For each loop which is mostly used for the Array in js..
+
+const coding=["js", "ruby","python", "java", 'c++']
+coding.forEach( function (elements){
+    console.log(elements);
+} )
+
+//Arrow Function Used:
+
+coding.forEach((elements2)=>{
+    console.log("This is of Element 2 : ");
+    console.log(elements2);
+})
