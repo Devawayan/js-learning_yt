d4fd30bb428b8265eaac8ae6378ecb67a39ce358
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-09-22 06:51:06 +0000

    For Each Loop In JS

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -11,3 +11,39 @@ coding.forEach((elements2)=>{
     console.log("This is of Element 2 : ");
     console.log(elements2);
 })
+
+//Using Refrence of a function written out of for each
+
+function print(item){
+    console.log(item);
+}
+
+//When we write only name of the Function it gives the refrence and when we give() it executes the function..
+
+coding.forEach(print)
+//This for each also contain many items at a time..
+//(items,index,arr)
+
+const myCoding=[
+    {
+        languageName: "JS",
+        languageUsed:"Coding Purpose",
+        languageFor:"Front-End Coding"
+    },
+    {
+        languageName: "Java",
+        languageUsed:"Coding Purpose",
+        languageFor:"Back-End Coding"
+    },
+    {
+        languageName: "Python",
+        languageUsed:"Coding Purpose",
+        languageFor:"Back-End Coding"
+    },
+]
+
+myCoding.forEach( (items)=>{
+    // console.log(items.languageFor);
+    console.log(items.languageName);
+    // console.log(items.languageUsed);
+} )
