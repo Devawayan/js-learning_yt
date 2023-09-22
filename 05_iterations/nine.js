72fc79026d1e9235afe0af85bd91ce8768996e3e
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2023-09-22 15:59:29 +0000

    DOM in Js

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -16,3 +16,28 @@ const total=arr.reduce( (acc,currval)=>{
 },0 )
 
 console.log(total);
+
+//Example 1:
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
